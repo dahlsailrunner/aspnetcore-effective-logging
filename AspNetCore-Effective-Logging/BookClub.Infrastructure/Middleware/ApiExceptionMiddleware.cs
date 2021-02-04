af6ab22a62e9092e4d3434762414bd6a51a3d2cf
Terry Aney <terry.aney@icloud.com>
2021-02-03 20:16:13 -0600

    Used EnsureSuccessStatusCodeHandler to simply allow exceptions to throw from API and rethrow in UI.  The SpanId in log events should now match for all events from UI and API.  Fixes #6

--- AspNetCore-Effective-Logging/BookClub.Infrastructure/Middleware/ApiExceptionMiddleware.cs
+++ AspNetCore-Effective-Logging/BookClub.Infrastructure/Middleware/ApiExceptionMiddleware.cs
@@ -1,67 +1,70 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
 using System.Net;
 using System.Threading.Tasks;
 
 namespace BookClub.Infrastructure.Middleware
 {
-    public class ApiExceptionMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<ApiExceptionMiddleware> _logger;
-        private readonly ApiExceptionOptions _options;
+	public class ApiExceptionMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ApiExceptionMiddleware> _logger;
+		private readonly ApiExceptionOptions _options;
 
-        public ApiExceptionMiddleware(ApiExceptionOptions options, RequestDelegate next, 
-            ILogger<ApiExceptionMiddleware> logger)
-        {
-            _next = next;
-            _logger = logger;
-            _options = options;
-        }
+		public ApiExceptionMiddleware( ApiExceptionOptions options, RequestDelegate next,
+			ILogger<ApiExceptionMiddleware> logger )
+		{
+			_next = next;
+			_logger = logger;
+			_options = options;
+		}
 
-        public async Task Invoke(HttpContext context /* other dependencies */)
-        {
-            try
-            {
-                await _next(context);
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(context, ex);
-            }
-        }
+		public async Task Invoke( HttpContext context /* other dependencies */)
+		{
+			try
+			{
+				await _next( context );
+			}
+			catch ( Exception ex )
+			{
+				HandleExceptionAsync( context, ex );
+				throw; // Just let exception be thrown so caller can process it
+			}
+		}
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
-        {
-            var error = new ApiError
-            {
-                Id = Guid.NewGuid().ToString(),
-                Status = (short)HttpStatusCode.InternalServerError,
-                Title = "Some kind of error occurred in the API.  Please use the id and contact our " +
-                        "support team if the problem persists."
-            };
-            
-            _options.AddResponseDetails?.Invoke(context, exception, error);            
+		private void HandleExceptionAsync( HttpContext context, Exception exception )
+		{
+			var error = new ApiError
+			{
+				Id = Guid.NewGuid().ToString(),
+				Status = (short)HttpStatusCode.InternalServerError,
+				Title = "Some kind of error occurred in the API.  Please use the id and contact our " +
+						"support team if the problem persists."
+			};
 
-            var innerExMessage = GetInnermostExceptionMessage(exception);
+			_options.AddResponseDetails?.Invoke( context, exception, error );
 
-            var level = _options.DetermineLogLevel?.Invoke(exception) ?? LogLevel.Error;
-            _logger.Log(level, exception, "BADNESS!!! " + innerExMessage  + " -- {ErrorId}.", error.Id);           
+			var innerExMessage = GetInnermostExceptionMessage( exception );
 
+			var level = _options.DetermineLogLevel?.Invoke( exception ) ?? LogLevel.Error;
+			_logger.Log( level, exception, "BADNESS!!! " + innerExMessage + " -- {ErrorId}.", error.Id );
+
+			// No longer returning 'text' to caller.  Will let exception throw.
+			/*
             var result = JsonConvert.SerializeObject(error);
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             return context.Response.WriteAsync(result);
-        }
+			*/
+		}
 
-        private string GetInnermostExceptionMessage(Exception exception)
-        {
-            if (exception.InnerException != null)
-                return GetInnermostExceptionMessage(exception.InnerException);
+		private string GetInnermostExceptionMessage( Exception exception )
+		{
+			if ( exception.InnerException != null )
+				return GetInnermostExceptionMessage( exception.InnerException );
 
-            return exception.Message;
-        }
-    }
+			return exception.Message;
+		}
+	}
 }
